3444c83e6c689fc5006ef4aaa9973e40054a5254
Indumathi-Arukkutty <95235524+Indumathi-Arukkutty@users.noreply.github.com>
2022-01-01 07:55:40 +0530

    JS-stop watch demo in

---
+++ Stopwatch/stopwatch.js
@@ -0,0 +1,70 @@
+var hr = 0;
+var min = 0;
+var sec = 0;
+var timer = document.querySelector('.watchtimer');
+var stoptime = true;
+
+function start(){
+    if(stoptime == true){
+        stoptime = false;
+        timecycle();
+    }
+}
+
+function timecycle()
+{
+    if(stoptime == false)
+    {
+      sec = parseInt(sec)
+      min = parseInt(min)
+      hr = parseInt(hr)
+  
+        console.log(typeof(sec));
+        console.log(typeof(min));
+        console.log(typeof(hr));
+        sec = sec+1;
+
+        if (sec == 60) {
+            min = min + 1;
+            sec = 0;
+          }
+          if (min == 60) {
+            hr = hr + 1;
+            min = 0;
+            sec = 0;
+          }
+      
+          if (sec < 10 || sec == 0) {
+            sec = '0' + sec;
+          }
+          if (min < 10 || min == 0) {
+            min = '0' + min;
+          }
+          if (hr < 10 || hr == 0) {
+            hr = '0' + hr;
+          }
+          
+        timer.innerHTML = hr + ':' + min + ':' + sec;
+      
+        setTimeout("timecycle()", 1000);
+      
+    }
+        
+}
+
+function stop()
+{
+    if (stoptime == false)
+    {
+      stoptime = true;
+    }
+}
+
+function reset()
+{
+    timer.innerHTML = '00:00:00'
+    stopTime = true
+    hr = 0
+    sec = 0
+    min = 0
+}
